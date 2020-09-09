5acc8541fc057e4a8b77da0fec500d0100eb1da6
Ahn-Ssu <21600402@handong.edu>
2020-09-09 22:40:30 +0900

    fixed bug

--- Hw1_p7.rkt
+++ Hw1_p7.rkt
@@ -1,15 +1,21 @@
 #lang plai
-(define (update-name target source theList)
-  (cond
-    [(empty? theList) theList]
-    [(not (index-of theList target)) theList]
-    [else
-      (update-name target source (append (list-set theList (index-of theList target) (string-append target source))))
-      ]
-   ) 
+(define-type Vehicle
+  [Bicycle (wheels (and (integer?) (not (negative?))))]
+  [Car (wheels integer?)
+       (windows integer?)]
+  [Airplane (wheels integer?)
+            (windows integer?)
+            (engines integer?)]
   )
 
-(test (update-name "claire" " is nice" '("jc" "claire" "kate")) '("jc" "claire is nice" "kate"))
-(test (update-name "ahnssu" " is nice" '("jc" "claire" "kate")) '("jc" "claire" "kate"))
-(test (update-name "csee" " is my major" '("handong" "edu" "csee" "hisnet" "CSEE")) '("handong" "edu" "csee is my major" "hisnet" "CSEE"))
-(test (update-name "jc" " is cool" '("jc" "jc" "kate" "jc" "ahnssu" "jc")) '("jc is cool" "jc is cool" "kate" "jc is cool" "ahnssu" "jc is cool"))
+
+(define (vehicle-tax theVehicle)
+  (type-case Vehicle theVehicle
+    [Bicycle (wheels) wheels]
+    [Car (wheels windows) (+ wheels windows)]
+    [Airplane (wheels windows engines) (+ wheels (+ windows engines))]
+    )
+  )
+(test (vehicle-tax (Airplane 5 5 5))15)
+(test (vehicle-tax (Bicycle 3)) 3)
+(test (vehicle-tax (Car 4 2)) 6)
