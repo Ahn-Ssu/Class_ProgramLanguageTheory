968e2e6d4368706bc3b309517c4d5afbae8fa9e5
Ahn-Ssu <21600402@handong.edu>
2020-10-04 20:05:25 +0900

    test L07

--- L07_practice.rkt
+++ L07_practice.rkt
@@ -12,7 +12,7 @@
   (match sexp
     [(? number?) (num sexp)]
     [(list '+ l r) (add (parse l)(parse r))]
-     
+    [(list '- l r) (sub (parse l)(parse r))]
     [(list 'with (list i v) e) (with i (parse v) (parse e))]
     [(? symbol?) (id sexp)]
     [else (error 'parse "bad syntax:~a" sexp)]
@@ -21,8 +21,14 @@
 (test (parse '{+ {- 3 4} 7}) (add (sub (num 3) (num 4)) (num 7)))
 (test (parse '{with {x 5} {+ 8 2}}) (with 'x (num 5) (add (num 8) (num 2))))
 (test (parse '{with {x 5} {+ x x}}) (with 'x (num 5) (add (id 'x) (id 'x))))
+(test (parse '{with {x 3} {with {y 4}{with {z 5}{+ x {+ y z}}}}})
+      (with 'x (num 3) (with 'y (num 4) (with 'z (num 5) (add (id 'x) (add (id 'y) (id 'z))))))
+      )
+               
 ; 우리가 identifier를 지정하면 지정된 대로 수행을 함 
 ; (test (parse '{* {x 5} {+ x x}}) (* 'x (num 5) (add (id 'x) (id 'x))))
+; 결국 인터프리터는 알려준 해독 방법으로
+; Concrete Syntaxㅡ를 읽고 난 후 AST를 생성하는 것이다.
 
 
 
@@ -33,15 +39,21 @@
     [num (n) wae]
     [add (l r) (add (subst l bound-id actual-value)(subst r bound-id actual-value))]
     [sub (l r) (sub (subst l bound-id actual-value)(subst r bound-id actual-value))]
-    [with (i v e) (with i (subst v bound-id actual-value)(if (symbol=? i bound-id)
-                                                             e (subst e bound-id actual-value)))]
+    [with (i v e) (with i (subst v bound-id actual-value) ; with i ( lhs , rhs ) 의 형태를 가지게 됨 
+                        (if (symbol=? i bound-id) e (subst e bound-id actual-value)))] ; if 문에서 조건이 참이면 왼항, 거짓이면 오른항 수행 
     [id (s) (if (symbol=? s bound-id) (num actual-value) wae)]
     )
   )
-
 (test (subst (with 'y (num 17) (id 'x)) 'x 10) (with 'y (num 17) (num 10)))
 (test (subst (with 'y (id 'x) (id 'y)) 'x 10) (with 'y (num 10) (id 'y)))
 (test (subst (with 'x (id 'y) (id 'x)) 'x 10) (with 'x (id 'y) (id 'x)))
+;(test (subst (with 'x (with 'y (with 'z ((id 'x) ( (id 'y) (id 'z)))) 'z 3) 'y 4) 'x 3)
+ ;     (add (num 3) (add (num 4) (num 5)))
+  ;    )
+(test (subst  (with 'x (num 3) (with 'y (num 4) (with 'z (num 5) (add (id 'x) (add (id 'y) (id 'z)))))) 'x 3)
+      (with 'x (num 3) (with 'y (num 4) (with 'z (num 5) (add (id 'x) (add (id 'y) (id 'z))))))
+      )
+(test (subst (with 'z (num 5) (add (id 'x) (add (id 'y) (id 'z)))) 'z 5) (num 5))
 
 
 ; [contract] interp : WAE -> number
@@ -56,9 +68,7 @@
   )
 
 ; questionEx
-
 (interp (with 'x (num 5) (add (num 1) (with 'y (id 'x) (id 'y)))))
 ;(interp (parse 'p))
 ;(add (num 1) (with 'y (id 'x) (id 'y))))
 
- 
