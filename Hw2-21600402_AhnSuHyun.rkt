011e5b20f2df03901a75454d88e56f71860866c9
Ahn-Ssu <21600402@handong.edu>
2020-10-07 10:55:44 +0900

    solved 1-a

--- Hw2-21600402_AhnSuHyun.rkt
+++ Hw2-21600402_AhnSuHyun.rkt
@@ -10,9 +10,14 @@
 
 (define-type PWAE
   [num (n number?)]
-  [op ]
-  [id]
-  [keyword]
-  [postfix (lhs PWAE?) (rhs PWAE?) (op PWAE-op?)]
-  [substitute]
-  
+  [op (type (or/c 'add 'sub))]; symbol=? s '+ '-
+  [id (name symbol?)]
+  [keyword (keyword-name symbol?)]
+  [postfix (lhs PWAE?) (rhs PWAE?) (op PWAE?)]
+  [substitute (id-name symbol?) (named-expr PWAE?)(body PWAE?)]
+ )
+
+(postfix (num 10) (num 1) (op 'sub))
+(postfix (num 10) (num 1) (op 'add))
+(substitute 'x (num 5) (postfix (id 'x) (id 'x) (op 'add)))
+;(postfix (num 10) (num 1) (op 'oppp))
