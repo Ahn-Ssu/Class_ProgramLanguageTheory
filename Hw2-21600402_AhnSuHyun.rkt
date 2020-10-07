60fb7271fc6a2a31841befaaa3976313c5f928da
Ahn-Ssu <21600402@handong.edu>
2020-10-07 11:14:52 +0900

    solved 1-b

--- Hw2-21600402_AhnSuHyun.rkt
+++ Hw2-21600402_AhnSuHyun.rkt
@@ -14,10 +14,29 @@
   [id (name symbol?)]
   [keyword (keyword-name symbol?)]
   [postfix (lhs PWAE?) (rhs PWAE?) (op PWAE?)]
-  [substitute (id-name symbol?) (named-expr PWAE?)(body PWAE?)]
+  [substitute (id-name symbol?) (named-expr PWAE?)(body PWAE?)(sub-keyword PWAE?)]
  )
 
 (postfix (num 10) (num 1) (op 'sub))
 (postfix (num 10) (num 1) (op 'add))
-(substitute 'x (num 5) (postfix (id 'x) (id 'x) (op 'add)))
+(substitute 'x (num 5) (postfix (id 'x) (id 'x) (op 'add))(keyword 'with))
 ;(postfix (num 10) (num 1) (op 'oppp))
+
+;[contract] parse: sexp -> PWAE
+(define (parse sexp)
+  (match sexp
+    [(? number?) (num sexp)]
+    [(list l r '+) (postfix (parse l) (parse r) (op 'add))]
+    [(list l r '-) (postfix (parse l) (parse r) (op 'sub))]
+    [(list (list i v) e 'with) (substitute  i (parse v) (parse e) (keyword 'with))] 
+    [(? symbol?) (id sexp)]
+    [else (error 'parse "bad syntax: ~a" sexp)]
+    )
+  )
+
+
+(parse '{3 4 -})
+(parse '{{3 4 -} 7 +})
+(parse '{{x 5} {x x +} with})
+(parse '{{x {5 4 +}} {x x +} with}) 
+    
